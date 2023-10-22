4c6d8760193dd2e4195098fc44a1dfaca92f560f
lshkap613 <121770053+lshkap613@users.noreply.github.com>
2023-10-22 13:53:26 -0400

    Update Test_game.java
    Currently includes a method that should probably go to GoFish class

--- src/Test_game.java
+++ src/Test_game.java
@@ -4,10 +4,11 @@ import java.util.*;
 
 public class Test_game {
 	public static void main(String[] args) {
-		// craete Scanner, list of players, deck, and game
+		// create Scanner, list of players, deck, and game
 		Scanner input = new Scanner(System.in);
 		List<Player> players = new ArrayList<Player>();
 		Deck deck = new Deck();
+		List<Card> discard = new ArrayList<Card>();
 		Game game = new Game(players);
 		
 		// Start game
@@ -61,47 +62,67 @@ public class Test_game {
 		
 		System.out.println("\nYou will go first\n");  // create y/n method?
 		
-		boolean cont = true;
-		while (cont = false) {
-			System.out.println("(set/ask)");
+		boolean cont = false;
+		while (cont == false) {
+			System.out.println("set/ask"); // clarify
 			String action = input.nextLine();
 			
-			//validation
+			// input validation
+			while (!action.equalsIgnoreCase("set") && !action.equalsIgnoreCase("ask")) {
+				System.out.println("Not a valid option. Please type \"set\" or \"ask\"");
+				action = input.nextLine();
+			}
 			
 			if (action.equalsIgnoreCase("set")) {
 				System.out.println("Enter the 4 numbers of the cards in the set, one at a time");
+				
 				// display hand?
+				
 				int[] setNumbers = new int[4];
 				for (int num=1; num<=4; num++) {
 					System.out.print(num + ": ");
 					setNumbers[num-1] = input.nextInt(); 
 				}
 				
-				// check if ranks are the same
-				if (player1.getHand().get(setNumbers[0]-1).getRank() 
-					== player1.getHand().get(setNumbers[1]-1).getRank()
-					&&
-					player1.getHand().get(setNumbers[1]-1).getRank()
-					== player1.getHand().get(setNumbers[2]-1).getRank()
-					&&
-					player1.getHand().get(setNumbers[2]-1).getRank()
-					== player1.getHand().get(setNumbers[3]-1).getRank()
-					) {
+				// check if 4 cards selected make a set
+				Card[] set = checkSet(player1, setNumbers);
+				
+				if (set != null) {
 					System.out.println("You have a set!");
-					// not done!
-					// If yes, all 4 to discard pile
-					cont = true;
+					
+					// discard set
+					for (Card card : set) {
+						player1.removeFromHand(card);
+						discard.add(card);
+					}
 					
 				} else {
 					System.out.println("Those cards do not make a set");
+
 				}
 	
-			}else if (action.equalsIgnoreCase("ask")) {
+			}else {
 				// ask code
-			} else {
-				// validation here or before.
 			}
 			
 		} // end while
 	} 
+	
+	public static Card[] checkSet(Player player, int[] setNumbers) {
+		Card card1 = player.getHand().get(setNumbers[0]-1);
+		Card card2 = player.getHand().get(setNumbers[1]-1);
+		Card card3 = player.getHand().get(setNumbers[2]-1);
+		Card card4 = player.getHand().get(setNumbers[3]-1);
+						
+		// check if ranks are the same
+		if (card1.getRank() == card2.getRank()
+			&& card2.getRank() == card3.getRank()
+			&& card3.getRank() == card4.getRank()) {
+			
+			Card [] set = {card1, card2, card3, card4};
+			return set;
+		} 
+		
+		return null;
+	}
 }
